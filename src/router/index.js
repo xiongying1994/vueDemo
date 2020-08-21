630d5cf5da366263f4a6cb41c1e99be144569826
yx_xiongying <xiongying@asiainfo.com>
2020-08-21 17:10:54 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 注册路由插件    如果使用模块化机制编程，导入Vue和VueRouter，要调用 Vue.use(VueRouter)
+Vue.use(Router)
+
+// base:"/"路由的前缀
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: () => import('@/components/HelloWorld')
+    },
+    {
+      path: '/excel',
+      name: 'excel',
+      component: () => import('@/components/Excel')
+    },
+    {
+      path: '/excel2',
+      name: 'excel2',
+      component: () => import('@/components/Excel2')
+    }
+  ]
+})
